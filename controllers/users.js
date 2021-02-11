c6b92308fac14ad8543a28f93223d09103e913da
yadu <yadu@ensurity.in>
2021-02-11 20:39:43 +0530

    ES6 changes

--- controllers/users.js
+++ controllers/users.js
@@ -7,34 +7,33 @@ let mongoose = require('mongoose'),
   logger = require('../lib/logger')
 
 exports.register = async function (req, res) {
-  let check_user = await User.find({email:req.body.email})
-  if(check_user && check_user.length){
+  let check_user = await User.find({
+    email: req.body.email
+  })
+  if (check_user && check_user.length) {
     return res.status(400).send({
       message: "User Already Present"
     });
   }
   let newUser = new User(req.body);
   newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
-  newUser.save(function (err, user) {
-    if (err) {
-      logger.error("Error On Creating User ",err)
-      return res.status(400).send({
-        message: err
-      });
-    } else {
-      user.hash_password = undefined;
-      return res.json(user);
-    }
-  });
+  try {
+    let user = await newUser.save()
+    user.hash_password = undefined;
+    return res.json(user);
+  } catch (error) {
+    logger.error("Error On Creating User ", error)
+    return res.status(400).send({
+      message: err
+    });
+  }
 };
 
-exports.sign_in = function (req, res) {
-  User.findOne({
-    email: req.body.email
-  }, function (err, user) {
-    if (err) {
-      logger.error("Error On Finding User")
-    } err;
+exports.sign_in = async function (req, res) {
+  try {
+    let user = await User.findOne({
+      email: req.body.email
+    })
     if (!user || !user.comparePassword(req.body.password)) {
       return res.status(401).json({
         message: 'Authentication failed. Invalid user or password.'
@@ -47,20 +46,27 @@ exports.sign_in = function (req, res) {
         _id: user._id
       }, 'RESTFULAPIs')
     });
-  });
+  } catch (error) {
+    logger.error("Error On Finding User")
+    return res.status(401).json({
+      message: 'Error On Finding User'
+    });
+  }
+
 };
 
-exports.loginRequired = function (req, res, next) {
+exports.loginRequired = async function (req, res, next) {
   if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-    jwt.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function (err, decode) {
-      if (err) {
-        return res.status(401).json({
-          message: 'Unauthorized user!!'
-        });
-      }
-      req.user = decode;
+    try {
+      let jwt_verify = await jwt.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs');
+      req.user = jwt_verify;
       next();
-    });
+    } catch (error) {
+      return res.status(401).json({
+        message: 'Unauthorized user!!'
+      });
+    }
+
   } else {
     return res.status(401).json({
       message: 'Unauthorized user!!'
@@ -68,4 +74,3 @@ exports.loginRequired = function (req, res, next) {
   }
 
 };
-
